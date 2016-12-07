7a8b71e2f75fbc5cb9aa8068de776b1cb6425806
Sama <gbozee@gmail.com>
2016-12-07 10:07:24 +0100

    changed to g_objects

--- tuteria_application_test/users/models.py
+++ tuteria_application_test/users/models.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
+
+class UserQuerySet(models.QuerySet):
+    pass
+
 
 
 @python_2_unicode_compatible
@@ -14,6 +19,7 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    g_objects = UserQuerySet.as_manager()
 
     def __str__(self):
         return self.username
@@ -25,7 +31,7 @@ class User(AbstractUser):
 class Booking(models.Model):
     user = models.ForeignKey(User, null=True)
     order = models.CharField(max_length=12, primary_key=True, db_index=True)
-    
+
 
 class Wallet(models.Model):
     owner = models.OneToOneField(User, related_name='wallet')
