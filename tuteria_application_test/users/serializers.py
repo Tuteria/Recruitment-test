6ad1b3865ab4ba3c9ce10233155919bc8dca03c0
deola <dorcasbalogun5@gmail.com>
2016-12-15 16:17:49 +0100

    updated views, serializers

--- tuteria_application_test/users/serializers.py
+++ tuteria_application_test/users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    booking_order = serializers.ListField(
-        child=serializers.CharField())
-    transaction_total = serializers.DecimalField(max_digits=8, decimal_places= 2)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'last_name', 'first_name', 'email')
+
+
