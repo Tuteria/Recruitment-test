bf7e940c04945640e5edae81941f9d05016dd288
johnpaulchukwu43 <johnpaulchukwu43@gmail.com>
2017-01-18 11:50:11 +0100

    Imported User serializer and UserApi View
    Please discard previous commits..this is final

--- tuteria_application_test/users/tests/test_views.py
+++ tuteria_application_test/users/tests/test_views.py
@@ -1,62 +1,120 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
 import json
-from django.core.urlresolvers import reverse
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView, View
-from braces.views import CsrfExemptMixin, JsonRequestResponseMixin, JSONResponseMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .serializer import UserSerializer
-from .models import User
+from django.test import RequestFactory
+from unittest.mock import patch
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from test_plus.test import TestCase
+from ..serializer import UserSerializer
+from .factories import UserFactory, BookingFactory, WalletTransactionFactory
+from ..views import (
+    UserRedirectView,
+    UserUpdateView,
+    UserApiView,
+)
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+class BaseUserTestCase(TestCase):
+
+    def setUp(self):
+        self.user = self.make_user()
+        self.factory = RequestFactory()
 
 
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-    permanent = False
+class TestUserRedirectView(BaseUserTestCase):
 
-    def get_redirect_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    fields = ['name', ]
-
-    # we already imported User in the view code above, remember?
-    model = User
-
-    # send the user back to their own page after a successful update
-    def get_success_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-    def get_object(self):
-        # Only get the User record for the user making the request
-        return User.objects.get(username=self.request.user.username)
+    def test_get_redirect_url(self):
+        # Instantiate the view directly. Never do this outside a test!
+        view = UserRedirectView()
+        # Generate a fake request
+        request = self.factory.get('/fake-url')
+        # Attach the user to the request
+        request.user = self.user
+        # Attach the request to the view
+        view.request = request
+        # Expect: '/users/testuser/', as that is the default username for
+        #   self.make_user()
+        self.assertEqual(
+            view.get_redirect_url(),
+            '/users/testuser/'
+        )
 
 
-class UserListView(LoginRequiredMixin, ListView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
+class TestUserUpdateView(BaseUserTestCase):
 
+    def setUp(self):
+        # call BaseUserTestCase.setUp()
+        super(TestUserUpdateView, self).setUp()
+        # Instantiate the view directly. Never do this outside a test!
+        self.view = UserUpdateView()
+        # Generate a fake request
+        request = self.factory.get('/fake-url')
+        # Attach the user to the request
+        request.user = self.user
+        # Attach the request to the view
+        self.view.request = request
 
-class UserApiView(CsrfExemptMixin, JsonRequestResponseMixin, View):
-     # require_json = True
+    def test_get_success_url(self):
+        # Expect: '/users/testuser/', as that is the default username for
+        #   self.make_user()
+        self.assertEqual(
+            self.view.get_success_url(),
+            '/users/testuser/'
+        )
 
-     def get(self, request, *args, **kwargs):
-         user = User.g_objects.filter(pk=kwargs['pk']).with_transaction_and_booking().first()
-         as_json = UserSerializer(user).data
-         return self.render_json_response(as_json)
+    def test_get_object(self):
+        # Expect: self.user, as that is the request's user object
+        self.assertEqual(
+            self.view.get_object(),
+            self.user
+        )
 
-     def post(self, request, *args, **kwargs):
-         email = json.loads(self.request_json)['email']
-         user = User.g_objects.filter(email=email).with_transaction_and_booking().first()
-         as_json = UserSerializer(user).data
-         return self.render_json_response(as_json)
+class DjangoRestFrameworkUsageApiTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = UserFactory(first_name='Biola', last_name='Oyeniyi',
+                                email='b_oye@example.com', id=1)
+        self.booking = BookingFactory(user=self.user, order='ABCDEFGHIJKL')
+        WalletTransactionFactory(booking=self.booking,
+                                 wallet=self.user.wallet, total=20000)
+        self.patch = patch('tuteria_application_test.users.views.UserSerializer')
+        self.mock = self.patch.start()
+        self.mock.return_value = UserSerializer(UserFactory.get_user(self.user))
+
+    def tearDown(self):
+        self.patch.stop()
+
+    def test_post_request_for_api_view(self):
+        data = {
+            "email": self.user.email,
+        }
+
+        url = self.reverse('users:the_api', self.user.pk)
+        response = self.json_post(data, url=url)
+
+        self.mock.assert_called_once_with(self.user)
+        data2 = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(data2, {
+            'first_name': 'Biola',
+            'last_name': 'Oyeniyi',
+            'booking_order': ['ABCDEFGHIJKL'],
+            'transaction_total': '20000.00'
+        })
+
+    def json_post(self, data, cls=UserApiView, url=None):
+        request = self.factory.post(
+            url, json.dumps(data), 'json',
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',)
+        return cls.as_view()(request)
+
+    def test_api_view_get_request_returns_valid_response(self):
+        response = self.client.get(self.reverse('users:the_api', self.user.pk))
+        self.mock.assert_called_once_with(self.user)
+        data = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(data, {
+            'first_name': 'Biola',
+            'last_name': 'Oyeniyi',
+            'booking_order': ['ABCDEFGHIJKL'],
+            'transaction_total': '20000.00'
+        })
