9ab479c46b73612a5321fd0bb53a582a1076ee21
johnpaulchukwu43 <johnpaulchukwu43@gmail.com>
2017-01-18 11:27:37 +0100

    Add files via upload

---
+++ tuteria_application_test/users/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    booking_order = serializers.SerializerMethodField()
+    transaction_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name',
+                  'booking_order', 'transaction_total']
+
+    def get_booking_order(self, obj):
+        return [x.order for x in obj.orders.all()]
+
+    def get_transaction_total(self, obj):
+       return obj.transaction_total
