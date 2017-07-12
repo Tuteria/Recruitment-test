d128cb351f656164a95cd990a8e50ae2dcdbe47b
pythonGeek <stephen.kanyi@andela.com>
2017-07-12 18:56:55 +0300

    adds the user serializer

--- tuteria_application_test/users/serializers.py
+++ tuteria_application_test/users/serializers.py
@@ -1,21 +1,23 @@
 from rest_framework import serializers
+from django.db.models import Sum
 from .models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the User model."""
+
     booking_order = serializers.SerializerMethodField()
     transaction_total = serializers.SerializerMethodField()
 
+    def get_booking_order(self, user):
+        return [order.order for order in user.orders.all()]
+
+    def get_transaction_total(self, user):
+        total = user.wallet.transactions.aggregate(total=Sum('total'))
+        
+        return str(total['total'])
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name',
-                  'booking_order', 'transaction_total']
-
-    @staticmethod
-    def get_booking_order(val):
-        return [booking.order for booking in val.orders.all()]
-
-
-    @staticmethod
-    def get_transaction_total(val):
-        return val.transaction_total
+        fields = (
+            'first_name', 'last_name', 'booking_order', 'transaction_total'
+            )
